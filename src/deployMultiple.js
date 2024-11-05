01f7a9aebe35f36adf2818645df3d334c1ef6f71
Syndrome Dev <135868855+Hamzahhamzah123@users.noreply.github.com>
2024-11-05 13:03:32 +0700

    Update deployMultiple.js

--- src/deployMultiple.js
+++ src/deployMultiple.js
@@ -2,14 +2,25 @@ const { deployContract } = require("./deploy");
 const fs = require("fs");
 const path = require("path");
 
-// Membaca konfigurasi jaringan dari file JSON di dalam direktori chains
+function getUserInput() {
+    // Nilai default untuk token
+    const defaultName = 'DefaultToken';
+    const defaultSymbol = 'DTK';
+    const defaultSupply = 1000000;
+
+    return {
+        name: defaultName,
+        symbol: defaultSymbol,
+        supply: defaultSupply,
+    };
+}
+
+// Mengambil nilai default dari getUserInput
+const { name, symbol, supply } = getUserInput();
+
+// Konfigurasi jaringan
 const networkConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "chains", "Testnet.json"), "utf8"));
 const network = networkConfig[0]; // Mengambil jaringan pertama dari array
-
-// Parameter token
-const name = "TokenName"; // Sesuaikan nama token Anda
-const symbol = "TKN"; // Sesuaikan simbol token Anda
-const supply = 1000000; // Adjust the supply as needed
 
 // Menjalankan deploy sebanyak 100 kali
 deployContract(network, name, symbol, supply, 100);
